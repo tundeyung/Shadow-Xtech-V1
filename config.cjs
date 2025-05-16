385873ebc1c42d020d33c925117f5dac53b9d3b3
Black-Tappy🏷️ <tappyblack1@gmail.com>
2025-05-16 14:51:11 +0300

    || Ⴊl𐌀Ꮳk𐌕𐌀ႲႲჄ

---
+++ config.cjs
@@ -0,0 +1,58 @@
+
+// Shadow-Xtech-V1 config.js
+const fs = require("fs");
+require("dotenv").config();
+
+const config = {
+  SESSION_ID: process.env.SESSION_ID || "POPKID~CYpjEQZJ#HcxxljkhqE8qSOl1YRCRe3KSEChy-_0CRNFCBQ7nHz4",
+  PREFIX: process.env.PREFIX || '.',
+//let's add menu captions by tappy black
+   BOT_NAME: process.env.BOT_NAME || "Sʜᴀᴅᴏᴡ-Xᴛᴇᴄʜ",
+   BOT: process.env.BOT || "hello 👋",
+  // lets add new cmd by tappy black
+  NEW_CMD: process.env.NEW_CMD || "ᴀᴅᴅᴠᴀʀ\n│ sᴜᴅᴏ\n| bright",
+  CAPTION: process.env.CAPTION|| "ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʙʟᴀᴄᴋ ᴛᴀᴘᴘʏ",
+// don't use my codes without a permission 🙏
+  AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true,
+  AUTO_BIO: process.env.AUTO_BIO !== undefined ? process.env.AUTO_BIO === 'true' : true,
+  AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT !== undefined ? process.env.AUTO_STATUS_REACT === 'true' : true,
+  /*anti left by tappy black tech*/
+  ANTI_LEFT: process.env.ANTI_LEFT !== undefined ? process.env.ANTI_LEFT === 'true' : true,
+  AUTOLIKE_EMOJI: process.env.AUTOLIKE_EMOJI || '🩷', // For liking status updates(stories)
+  AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS !== undefined ? process.env.AUTO_REPLY_STATUS === 'true' : false,
+  STATUS_READ_MSG: process.env.STATUS_READ_MSG || 'Status Viewed By Sʜᴀᴅᴏᴡ-Xᴛᴇᴄʜ',
+  VOICE_CHAT_BOT: process.env.VOICE_CHAT_BOT !== undefined ? process.env.VOICE_CHAT_BOT === 'true' : false,
+  AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT !== undefined ? process.env.AUTO_STATUS_REACT === 'true' : true,
+  ANTILINK: process.env.ANTILINK !== undefined ? process.env.ANTILINK === 'true' : false,
+  ANTI_LEFT: process.env.ANTI_LEFT !== undefined ? process.env.ANTI_LEFT === 'true' : true,
+  AUTO_STICKER: process.env.AUTO_STICKER !== undefined ? process.env.AUTO_STICKER === 'true' : false,
+  AUTO_READ: process.env.AUTO_READ !== undefined ? process.env.AUTO_READ === 'true' : false,
+  AUTO_TYPING: process.env.AUTO_TYPING !== undefined ? process.env.AUTO_TYPING === 'true' : false,
+  AUTO_RECORDING: process.env.AUTO_RECORDING !== undefined ? process.env.AUTO_RECORDING === 'true' : false,
+  ALWAYS_ONLINE: process.env.ALWAYS_ONLINE !== undefined ? process.env.ALWAYS_ONLINE === 'true' : false,
+  AUTO_REACT: process.env.AUTO_REACT !== undefined ? process.env.AUTO_REACT === 'true' : false,
+   /*auto block only for 212 */
+  AUTO_BLOCK: process.env.AUTO_BLOCK !== undefined ? process.env.AUTO_BLOCK === 'true' : true,
+  /*tappy black tech added anti delete cmd*/
+  ANTI_DELETE: process.env.ANTI_DELETE !== undefined ? process.env.ANTI_DELETE === 'true' : false,
+  /*lets add auto bot respond*/
+  CHAT_BOT: process.env.CHAT_BOT !== undefined ? process.env.CHAT_BOT === 'true' : false,
+  /*lets add chat bot mode*/
+  CHAT_BOT_MODE: process.env.CHAT_BOT_MODE || "public",
+  /*how about Lydia chat bot*/
+  LYDEA: process.env.LYDEA !== undefined ? process.env.LYDEA === 'true' : false,
+  REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
+  NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
+  MODE: process.env.MODE || "public",
+  DELETED_MESSAGES_CHAT_ID: process.env.DELETED_MESSAGES_CHAT_ID || "254759000340@s.whatsapp.net",
+  OWNER_NAME: process.env.OWNER_NAME || "Ⴊl𐌀Ꮳk𐌕𐌀ႲႲჄ",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "254759000340",
+  SUDO_NUMBER: process.env.SUDO_NUMBER || "254759000340",
+  GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
+  WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
+};
+
+
+module.exports = config;
+
+  
---
+++ index.js
@@ -0,0 +1,299 @@
+/*
+
+made by Black-Tappy
+contact owner +254759000340
+*/
+
+
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+import {
+    makeWASocket,
+    Browsers,
+    fetchLatestBaileysVersion,
+    DisconnectReason,
+    useMultiFileAuthState,
+    getContentType
+} from '@whiskeysockets/baileys';
+import { Handler, Callupdate, GroupUpdate } from './shadowXtappy/shadowXevents/shadow.js';
+import express from 'express';
+import pino from 'pino';
+import fs from 'fs';
+import { File } from 'megajs';
+import NodeCache from 'node-cache';
+import path from 'path';
+import chalk from 'chalk';
+import moment from 'moment-timezone';
+import axios from 'axios';
+import config from './config.cjs';
+import pkg from './lib/autoreact.cjs';
+
+const { emojis, doReact } = pkg;
+const prefix = process.env.PREFIX || config.PREFIX;
+const sessionName = "session";
+const app = express();
+const orange = chalk.bold.hex("#FFA500");
+const lime = chalk.bold.hex("#32CD32");
+let useQR = false;
+let initialConnection = true;
+const PORT = process.env.PORT || 3000;
+
+const MAIN_LOGGER = pino({
+    timestamp: () => `,"time":"${new Date().toJSON()}"`
+});
+const logger = MAIN_LOGGER.child({});
+logger.level = "trace";
+
+const msgRetryCounterCache = new NodeCache();
+
+const __filename = decodeURIComponent(new URL(import.meta.url).pathname);
+const __dirname = path.dirname(__filename);
+
+const sessionDir = path.join(__dirname, 'session');
+const credsPath = path.join(sessionDir, 'creds.json');
+
+if (!fs.existsSync(sessionDir)) {
+    fs.mkdirSync(sessionDir, { recursive: true });
+}
+
+async function downloadSessionData() {
+    console.log("Debugging SESSION_ID:", config.SESSION_ID);
+
+    if (!config.SESSION_ID) {
+        console.error('❌ Please add your session to SESSION_ID env !!');
+        return false;
+    }
+
+    const sessdata = config.SESSION_ID.split("POPKID~")[1];
+
+    if (!sessdata || !sessdata.includes("#")) {
+        console.error('❌ Invalid SESSION_ID format! It must contain both file ID and decryption key.');
+        return false;
+    }
+
+    const [fileID, decryptKey] = sessdata.split("#");
+
+    try {
+        console.log("♻️ Downloading Session...");
+        const file = File.fromURL(`https://mega.nz/file/${fileID}#${decryptKey}`);
+
+        const data = await new Promise((resolve, reject) => {
+            file.download((err, data) => {
+                if (err) reject(err);
+                else resolve(data);
+            });
+        });
+
+        await fs.promises.writeFile(credsPath, data);
+        console.log("🔒 Session Successfully Loaded !!");
+        return true;
+    } catch (error) {
+        console.error('❌ Failed to download session data:', error);
+        return false;
+    }
+}
+
+//update autobio
+const lifeQuotes = [
+  "The only way to do great work is to love what you do.",
+  "Strive not to be a success, but rather to be of value.",
+  "The mind is everything. What you think you become.",
+  "The best time to plant a tree was 20 years ago. The second best time is now.",
+  "Life is what happens when you're busy making other plans.",
+  "Be the change that you wish to see in the world.",
+  "The future belongs to those who believe in the beauty of their dreams.",
+  "It is never too late to be what you might have been.",
+  "Do not wait to strike till the iron is hot; but make the iron hot by striking.",
+  "The journey of a thousand miles begins with a single step."
+];
+
+async function updateBio(Matrix) {
+  try {
+    const now = moment().tz('Africa/Nairobi');
+    const time = now.format('HH:mm:ss');
+    const randomIndex = Math.floor(Math.random() * lifeQuotes.length);
+    const randomQuote = lifeQuotes[randomIndex];
+    const bio = `✨🌇 ꜱʜᴀᴅᴏᴡ xᴛᴇᴄʜ ɪs ✨ ᴀᴄᴛɪᴠᴇ ✨ 🟢 | ⏳ ${time} | 📸 | 💬🌆  | ${randomQuote}`;
+    await Matrix.updateProfileStatus(bio);
+    console.log(chalk.yellow(`ℹ️ Bio updated to: "${bio}"`));
+  } catch (error) {
+    console.error(chalk.red('Failed to update bio:'), error);
+  }
+}
+
+async function updateLiveBio(Matrix) {
+  try {
+   const now = moment().tz('Africa/Nairobi');
+    const time = now.format('HH:mm:ss');
+    const randomIndex = Math.floor(Math.random() * lifeQuotes.length);
+    const randomQuote = lifeQuotes[randomIndex];
+    const bio = `✨🌇 ꜱʜᴀᴅᴏᴡ xᴛᴇᴄʜ ɪs ✨ ᴀᴄᴛɪᴠᴇ ✨ 🟢 | ⏳ ${time} | 📸 | 💬🌆  | ${randomQuote}`;
+    await Matrix.updateProfileStatus(bio);
+    console.log(chalk.yellow(`ℹ️ Bio updated to: "${bio}"`));
+  } catch (error) {
+    console.error(chalk.red('Failed to update bio:'), error);
+  }
+}
+
+async function getStartingMessageData() {
+    try {
+        const response = await axios.get('https://joel-xmd-starting-message-apis.vercel.app/');
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching starting message data:', error);
+        return null;
+    }
+}
+
+async function start() {
+    try {
+        const { state, saveCreds } = await useMultiFileAuthState(sessionDir);
+        const { version, isLatest } = await fetchLatestBaileysVersion();
+        console.log(`Shadow-Xtech using WA v${version.join('.')}, isLatest: ${isLatest}`);
+
+        const Matrix = makeWASocket({
+            version,
+            logger: pino({ level: 'silent' }),
+            printQRInTerminal: useQR,
+            browser: ["Sʜᴀᴅᴏᴡ-Xᴛᴇᴄʜ, "safari", "3.3"],
+            auth: state,
+            getMessage: async (key) => {
+                return { conversation: "shadow xtech whatsapp user bot" };
+            }
+        });
+
+        Matrix.ev.on('connection.update', async (update) => {
+            const { connection, lastDisconnect } = update;
+            if (connection === 'close') {
+                if (lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut) {
+                    start();
+                }
+            } else if (connection === 'open') {
+                if (initialConnection) {
+                    console.log(chalk.green("✔️  ꜱʜᴀᴅᴏᴡ xᴛᴇᴄʜ ɪs ɴᴏᴡ ᴏɴʟɪɴᴇ ᴀɴᴅ ᴘᴏᴡᴇʀᴇᴅ ᴜᴘ"));
+
+                    const startingMessageData = await getStartingMessageData();
+
+                    if (startingMessageData) {
+                        const { title, bot_name, creator, thumbnail, image, channel_link, channel_jid, caption } = startingMessageData;
+
+                        const messagePayload = {
+                            image: { url: image },
+                            caption: caption || title,
+                            contextInfo: {
+                                isForwarded: true,
+                                forwardingScore: 999,
+                                forwardedNewsletterMessageInfo: {
+                                    newsletterJid: channel_jid,
+                                    newsletterName: bot_name,
+                                    serverMessageId: -1,
+                                },
+                                externalAdReply: {
+                                    title: bot_name,
+                                    body: "ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʙʟᴀᴄᴋ-ᴛᴀᴘᴘʏ",
+                                    thumbnailUrl: thumbnail,
+                                    sourceUrl: channel_link,
+                                    mediaType: 1,
+                                    renderLargerThumbnail: false,
+                                },
+                            },
+                        };
+
+                        await Matrix.sendMessage(Matrix.user.id, messagePayload);
+                    }
+
+                    initialConnection = false;
+                } else {
+                    console.log(chalk.blue("♻️ Connection reestablished after restart."));
+                }
+            }
+        });
+
+        Matrix.ev.on('creds.update', saveCreds);
+        Matrix.ev.on("messages.upsert", async chatUpdate => {
+            await Handler(chatUpdate, Matrix, logger);
+
+            try {
+                const mek = chatUpdate.messages[0];
+                if (!mek.key.fromMe && config.AUTO_REACT && mek.message) {
+                    const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
+                    await doReact(randomEmoji, mek, Matrix);
+                }
+
+                if (mek && mek.key.remoteJid === 'status@broadcast' && config.AUTO_STATUS_REACT === "true") {
+                    const jawadlike = await Matrix.decodeJid(Matrix.user.id);
+                    const emojiList = ['❤️', '💸', '😇', '🍂', '💥', '💯', '🔥', '💫', '💎', '💗'];
+                    const randomEmoji = emojiList[Math.floor(Math.random() * emojiList.length)];
+
+                    await Matrix.sendMessage(mek.key.remoteJid, {
+                        react: {
+                            text: randomEmoji,
+                            key: mek.key,
+                        }
+                    }, { statusJidList: [mek.key.participant, jawadlike] });
+
+                    console.log(`Auto-reacted to a status with: ${randomEmoji}`);
+                }
+            } catch (err) {
+                console.error('Error during message processing or auto-reaction:', err.stack);
+            }
+        });
+
+        Matrix.ev.on("call", async (json) => await Callupdate(json, Matrix));
+        Matrix.ev.on("group-participants.update", async (messag) => await GroupUpdate(Matrix, messag));
+
+        Matrix.public = config.MODE === "public";
+    } catch (error) {
+        console.error('Critical Error:', error);
+        process.exit(1);
+    }
+}
+
+async function init() {
+    if (fs.existsSync(credsPath)) {
+        console.log("🔒 Session file found, proceeding without QR code.");
+        await start();
+    } else {
+        const sessionDownloaded = await downloadSessionData();
+        if (sessionDownloaded) {
+            console.log("🔒 Session downloaded, starting bot.");
+            await start();
+        } else {
+            console.log("No session found or downloaded, QR code will be printed for authentication.");
+            useQR = true;
+            await start();
+        }
+    }
+}
+
+init();
+
+// EXPRESS ROUTES
+const htmlDir = path.join(__dirname, 'mydata', 'joeljames');
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(htmlDir, 'index.html'));
+});
+
+app.get('/inf', (req, res) => {
+    res.sendFile(path.join(htmlDir, 'info.html'));
+});
+
+app.get('/s', (req, res) => {
+    res.sendFile(path.join(htmlDir, 'support.html'));
+});
+
+app.get('/uses', (req, res) => {
+    res.sendFile(path.join(htmlDir, 'uses.html'));
+});
+
+// 404 fallback
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(htmlDir, '404.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
