7d900f7b2c27cc794a39a9fa30257305c3988907
Black-Tappy🏷️ <tappyblack1@gmail.com>
2025-05-18 13:09:48 +0300

    || Ⴊl𐌀Ꮳk𐌕𐌀ႲႲჄ

--- plugins/ping.js
+++ plugins/ping.js
@@ -32,7 +32,7 @@ async (conn, mek, m, { from, quoted, sender, reply }) => {
         const end = new Date().getTime();
         const responseTime = (end - start) / 1000;
 
-        const text = `> *Sʜᴀᴅᴏᴡ-Xᴛᴇᴄʜ Qᴜᴀɴᴛᴜᴍ Sᴘᴇᴇᴅ: ${responseTime.toFixed(2)}ms ${reactionEmoji}*`;
+        const text = `> *Sʜᴀᴅᴏᴡ-Xᴛᴇᴄʜ Sᴘᴇᴇᴅ: ${responseTime.toFixed(2)}ms ${reactionEmoji}*`;
 
         await conn.sendMessage(from, {
             text,
